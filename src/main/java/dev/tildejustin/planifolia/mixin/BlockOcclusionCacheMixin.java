89e37c4532373b030f5ce12006c58716d8bad687
tildejustin <spacepepper3.14@gmail.com>
2024-07-02 18:08:51 -0400

    port features from sodium 2.2.1

---
+++ src/main/java/dev/tildejustin/planifolia/mixin/BlockOcclusionCacheMixin.java
@@ -0,0 +1,19 @@
+package dev.tildejustin.planifolia.mixin;
+
+import com.llamalad7.mixinextras.sugar.Local;
+import me.jellysquid.mods.sodium.client.render.occlusion.BlockOcclusionCache;
+import net.minecraft.block.BlockState;
+import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(value = BlockOcclusionCache.class, targets = "me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockOcclusionCache")
+public class BlockOcclusionCacheMixin {
+    @Dynamic
+    @Inject(method = {"shouldDrawSide", "Lme/jellysquid/mods/sodium/client/render/chunk/compile/pipeline/BlockOcclusionCache;shouldDrawSide(Lnet/minecraft/class_2680;Lnet/minecraft/class_1922;Lnet/minecraft/class_2338;Lnet/minecraft/class_2350;)Z"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isSideInvisible(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/Direction;)Z"), cancellable = true, require = 0)
+    private void adjStateNullCheck(CallbackInfoReturnable<Boolean> cir, @Local(ordinal = 1) BlockState adjState) {
+        if (adjState == null) {
+            cir.setReturnValue(false);
+        }
+    }
+}
