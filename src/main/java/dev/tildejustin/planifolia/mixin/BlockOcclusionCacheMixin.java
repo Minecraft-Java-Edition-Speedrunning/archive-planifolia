a9b73624a0c7a54bc848a569fa12599217ea7d04
tildejustin <spacepepper3.14@gmail.com>
2024-07-19 12:59:03 -0400

    add require to make sure mixin is applied

--- src/main/java/dev/tildejustin/planifolia/mixin/BlockOcclusionCacheMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/BlockOcclusionCacheMixin.java
@@ -10,7 +10,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(value = BlockOcclusionCache.class, targets = "me.jellysquid.mods.sodium.client.render.chunk.compile.pipeline.BlockOcclusionCache")
 public class BlockOcclusionCacheMixin {
     @Dynamic
-    @Inject(method = {"shouldDrawSide", "shouldDrawSide(Lnet/minecraft/class_2680;Lnet/minecraft/class_1922;Lnet/minecraft/class_2338;Lnet/minecraft/class_2350;)Z"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isSideInvisible(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/Direction;)Z"), cancellable = true, require = 0)
+    @Inject(
+            method = {"shouldDrawSide", "shouldDrawSide(Lnet/minecraft/class_2680;Lnet/minecraft/class_1922;Lnet/minecraft/class_2338;Lnet/minecraft/class_2350;)Z"},
+            at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;isSideInvisible(Lnet/minecraft/block/BlockState;Lnet/minecraft/util/math/Direction;)Z"), cancellable = true, require = 1
+    )
     private void adjStateNullCheck(CallbackInfoReturnable<Boolean> cir, @Local(ordinal = 1) BlockState adjState) {
         if (adjState == null) {
             cir.setReturnValue(false);
