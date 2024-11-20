aaca288e84c188a4d72b87fa4632f5e121d31138
tildejustin <spacepepper3.14@gmail.com>
2024-11-20 08:54:49 -0500

    port to minecraft 1.21.3

--- src/main/java/dev/tildejustin/planifolia/mixin/SodiumWorldRendererMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/SodiumWorldRendererMixin.java
@@ -1,27 +1,26 @@
 package dev.tildejustin.planifolia.mixin;
 
+import com.llamalad7.mixinextras.sugar.Share;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
+import dev.tildejustin.planifolia.mixin.accessor.RenderPhaseAccessor;
 import net.caffeinemc.mods.sodium.client.render.SodiumWorldRenderer;
 import net.caffeinemc.mods.sodium.client.render.chunk.ChunkRenderMatrices;
-import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.render.RenderLayer;
-import org.spongepowered.asm.mixin.*;
+import net.minecraft.util.profiler.*;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.*;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SodiumWorldRenderer.class)
 public class SodiumWorldRendererMixin {
-    @Shadow
-    @Final
-    private MinecraftClient client;
-
     @Inject(method = "drawChunkLayer", at = @At("HEAD"), remap = false)
-    private void startProfiler(RenderLayer renderLayer, ChunkRenderMatrices matrices, double x, double y, double z, CallbackInfo ci) {
-        this.client.getProfiler().push("filterempty");
-        this.client.getProfiler().swap(() -> "render_" + renderLayer);
+    private void startProfiler(RenderLayer renderLayer, ChunkRenderMatrices matrices, double x, double y, double z, CallbackInfo ci, @Share("scopedProfiler") LocalRef<ScopedProfiler> scopedProfiler) {
+        scopedProfiler.set(Profilers.get().scoped(() -> "render_" + ((RenderPhaseAccessor) renderLayer).getName()));
+        scopedProfiler.get().addLabel(renderLayer::toString);
     }
 
     @Inject(method = "drawChunkLayer", at = @At("TAIL"), remap = false)
-    private void endProfiler(CallbackInfo ci) {
-        this.client.getProfiler().pop();
+    private void endProfiler(CallbackInfo ci, @Share("scopedProfiler") LocalRef<ScopedProfiler> scopedProfiler) {
+        scopedProfiler.get().close();
     }
 }
--- src/main/java/dev/tildejustin/planifolia/mixin/WorldRendererMixin.java
+++ src/main/java/dev/tildejustin/planifolia/mixin/WorldRendererMixin.java
@@ -4,13 +4,13 @@ import com.llamalad7.mixinextras.injector.wrapoperation.*;
 import dev.tildejustin.planifolia.mixin.accessor.SodiumWorldRendererAccessor;
 import net.caffeinemc.mods.sodium.client.render.SodiumWorldRenderer;
 import net.minecraft.client.render.WorldRenderer;
-import org.spongepowered.asm.mixin.*;
+import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 
 @Mixin(WorldRenderer.class)
 public abstract class WorldRendererMixin {
-    @WrapOperation(method = "getEntitiesDebugString", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/WorldRenderer;regularEntityCount:I"))
-    private int hidEntityCount(WorldRenderer instance, Operation<Integer> original) {
+    @WrapOperation(method = "getEntitiesDebugString", at = @At(value = "FIELD", target = "Lnet/minecraft/client/render/WorldRenderer;renderedEntitiesCount:I"))
+    private int hideEntityCount(WorldRenderer instance, Operation<Integer> original) {
         return ((SodiumWorldRendererAccessor) SodiumWorldRenderer.instance()).getUseEntityCulling() ? -1 : original.call(instance);
     }
 }
