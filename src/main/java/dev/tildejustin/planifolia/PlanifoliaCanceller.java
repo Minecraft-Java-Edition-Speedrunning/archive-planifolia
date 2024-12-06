a4fd195b8edebeb1e6f4b8bc11ac7b6d8c10c101
tildejustin <spacepepper3.14@gmail.com>
2024-12-06 11:10:00 -0500

    backport datapack support removal when just subjaring fabric resource api

--- src/main/java/dev/tildejustin/planifolia/PlanifoliaCanceller.java
+++ src/main/java/dev/tildejustin/planifolia/PlanifoliaCanceller.java
@@ -1,6 +1,7 @@
 package dev.tildejustin.planifolia;
 
 import com.bawnorton.mixinsquared.api.MixinCanceller;
+import net.fabricmc.loader.api.FabricLoader;
 
 import java.util.*;
 
@@ -9,6 +10,9 @@ public class PlanifoliaCanceller implements MixinCanceller {
     public boolean shouldCancel(List<String> targetClassNames, String mixinClassName) {
         return Arrays.asList(
                 "me.jellysquid.mods.sodium.mixin.features.options.MixinOptionsScreen"
-        ).contains(mixinClassName);
+        ).contains(mixinClassName) || Arrays.asList(
+                "net.fabricmc.fabric.mixin.resource.loader.MainMixin",
+                "net.fabricmc.fabric.mixin.resource.loader.client.CreateWorldScreenMixin"
+        ).contains(mixinClassName) && !FabricLoader.getInstance().isModLoaded("fabric");
     }
 }
